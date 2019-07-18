4d1d1decb2f18f10412ed671815388a08bb75c07
yamashush <38120991+yamashush@users.noreply.github.com>
2019-07-18 22:14:10 +0900

    :+1: add members search method

--- lib/esa_piyo.rb
+++ lib/esa_piyo.rb
@@ -1,5 +1,6 @@
 require "esa_piyo/version"
 require "esa"
+require "json"
 
 module EsaPiyo
   class Error < StandardError; end
@@ -7,6 +8,54 @@ module EsaPiyo
   class Client 
     def initialize(access_token: nil, current_team: nil)
       @client = Esa::Client.new(access_token: access_token, current_team: current_team)
+    end
+
+    def screen_name(email: nil)
+
+      params = {
+        page: 1,
+        per_page: 100,
+      }
+
+      begin
+        res = @client.members(params)
+        body = res.body
+        members = body["members"]
+
+        members.each do |member| 
+          if member["email"] == email then
+            return member["screen_name"]
+          end
+        end
+
+        params[:page] = body["next_page"]
+      end until params[:page].nil?
+
+      return 
+    end
+
+    def email(screen_name: nil)
+
+      params = {
+        page: 1,
+        per_page: 100,
+      }
+
+      begin
+        res = @client.members(params)
+        body = res.body
+        members = body["members"]
+
+        members.each do |member| 
+          if member["screen_name"] == screen_name then
+            return member["email"]
+          end
+        end
+
+        params[:page] = body["next_page"]
+      end until params[:page].nil?
+
+      return 
     end
   end
 end

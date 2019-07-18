162707fab5757452fea9ca4d8e755b05893b2617
yamashush <38120991+yamashush@users.noreply.github.com>
2019-07-18 18:30:02 +0900

    :+1: add esa-ruby client

--- lib/esa_piyo.rb
+++ lib/esa_piyo.rb
@@ -1,6 +1,12 @@
 require "esa_piyo/version"
+require "esa"
 
 module EsaPiyo
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client 
+    def initialize(access_token: nil, current_team: nil)
+      @client = Esa::Client.new(access_token: access_token, current_team: current_team)
+    end
+  end
 end
